327ab7ff6031e86a0fa2a5a98b1e86934510f6ad
Isaiah Dominic <ziyaetuk@gmail.com>
2018-06-27 11:46:40 +0100

    Commit of CSS, index.html, test part of app.js and sample service worker

--- js/app.js
+++ js/app.js
@@ -1,8 +1,37 @@
 //load API from currency concerter
 const api = `https://free.currencyconverterapi.com/api/v5/countries`;  
 
-let currencyType;
+fetch(api).then(
+    function(response){
+        if (response.status!==200){
+            console.log(`Fail to fetch data  ${response.status}`);
+            return;
+        }
 
+        response.json().then(
+            function(data){
+                let currency = data.results;
+                console.log(currency);
 
+                for (c in currency)
+                {
+                    let option = document.createElement('option');
+                    option.value =`${currency[c].id}`;
+                    let check = currency[c].currencySymbol;
+                    if(typeof check === 'undefined'){
+                        check = '';
+                        console.log(check);
+                    }
+                    option.text = `${check} (${currency[c].currencyName})`;
+                    expect.appendChild(option);
+                    have.appendChild(option.cloneNode(true));
 
-console.log(api); 
+                }
+
+            });
+    }
+)
+.catch(function(err){
+    console.log(err);
+}
+);
